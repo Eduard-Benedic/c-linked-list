d87e7c9bb2fcf49f0225a5af61ea9849eb108eb4
Eduard Benedic <46683608+Eduard-Benedic@users.noreply.github.com>
2024-06-09 21:18:49 +0100

    print

--- main.c
+++ main.c
@@ -5,10 +5,27 @@ typedef struct node {
     struct node *next;
 } Node;
 
+void print_list(Node *head) {
+    Node *current;
+    current = head;
+    int i = 0;
+    while (current != NULL) {
+        printf("Node %d: %d\n", i, current->value);
+        i++;
+        current = current->next;
+    }
+}
+
 int main()
 {
-    Node a;
+    Node a, b, c;
     a.value = 5;
+    b.value = 6;
+    c.value = 7;
 
-    printf("Node a.value: %d\n", a.value);
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
+
+    print_list(&a);
 }
