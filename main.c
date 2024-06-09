6c09ce613fd23f9c47d401eeace3bda712454e23
Eduard Benedic <46683608+Eduard-Benedic@users.noreply.github.com>
2024-06-09 20:44:08 +0100

    declare node

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct node {
+    int value;
+    struct node *next;
+} Node;
+
+int main()
+{
+    Node a;
+    a.value = 5;
+
+    printf("Node a.value: %d\n", a.value);
+}
